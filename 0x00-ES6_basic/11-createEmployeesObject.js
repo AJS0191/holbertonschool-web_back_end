bbb89387806c8788db9ab5b03e01499cd9aa8a2c
AJS0191 <ashtonjamess@live.com>
2022-05-02 21:55:05 -0700

    UPDATE: indents

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,8 +1,8 @@
 export default function createEmployeesObject(departmentName, employees) {
-    const deptList = {
-	[departmentName] : [
-	    ...employees,
-	],
-    }
-    return deptList
+  const deptList = {
+    [departmentName]: [
+      ...employees,
+    ],
+  };
+  return deptList;
 }
