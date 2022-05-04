dc89a462e5bf7509197133620df773fa0e867bbd
AJS0191 <ashtonjamess@live.com>
2022-05-04 11:09:20 -0700

    UPDATE: indents; define helpers before

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,13 @@
-export default function handleResponseFromAPI(promise) {
-    promise.then(apiResolve, apiReject)
-}
 const apiResolve = () => {
-    console.log("Got a response from the API")
-    return { status: 200, body: 'Success' }
-}
+  console.log('Got a response from the API');
+  return { status: 200, body: 'Success' };
+};
 
 const apiReject = () => {
-    console.log("Got a response from the API")
-    return new Error()
+  console.log('Got a response from the API');
+  return new Error();
+};
+
+export default function handleResponseFromAPI(promise) {
+  promise.then(apiResolve, apiReject);
 }
