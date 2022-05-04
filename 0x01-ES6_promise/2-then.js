617d2a84a6893c53980a9872daabdfec2004b4db
AJS0191 <ashtonjamess@live.com>
2022-05-04 11:05:20 -0700

    ADD: 2-then.js

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,12 @@
+export default function handleResponseFromAPI(promise) {
+    promise.then(apiResolve, apiReject)
+}
+const apiResolve = () => {
+    console.log("Got a response from the API")
+    return { status: 200, body: 'Success' }
+}
+
+const apiReject = () => {
+    console.log("Got a response from the API")
+    return new Error()
+}
