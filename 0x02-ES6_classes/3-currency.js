3aa531f74f7e0c8b5c9bd537e8399ea401e40857
AJS0191 <ashtonjamess@live.com>
2022-05-09 19:30:07 -0700

    ADD: 3-currency

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,33 @@
+export default class Currency {
+    constructor(code, name) {
+	if (typeof (code) === 'string') {
+	    this._code = code;
+	}
+	if (typeof (name) === 'string') {
+	    this._name = name;
+	}
+    }
+    get name() {
+	return this._name;
+    }
+    
+    get code() {
+	return this._code;
+    }
+    
+    set name(name) {
+	if (typeof (name) === 'string') {
+	    this._name = name;
+	}
+    }
+
+    set code(code) {
+	if (typeof (code) === 'string') {
+	    this._code = code;
+	}
+    }
+    
+    displayFullCurrency() {
+	return `${this._name} (${this._code})`;
+    }
+}
