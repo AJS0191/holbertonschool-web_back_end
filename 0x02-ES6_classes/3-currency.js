943fa04badd14260abca1b1c5124aee15139f2aa
AJS0191 <ashtonjamess@live.com>
2022-05-10 13:56:30 -0700

    FIX: eslint

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,33 +1,34 @@
 export default class Currency {
-    constructor(code, name) {
-	if (typeof (code) === 'string') {
-	    this._code = code;
-	}
-	if (typeof (name) === 'string') {
-	    this._name = name;
-	}
+  constructor(code, name) {
+    if (typeof (code) === 'string') {
+      this._code = code;
     }
-    get name() {
-	return this._name;
+    if (typeof (name) === 'string') {
+      this._name = name;
     }
-    
-    get code() {
-	return this._code;
-    }
-    
-    set name(name) {
-	if (typeof (name) === 'string') {
-	    this._name = name;
-	}
-    }
+  }
 
-    set code(code) {
-	if (typeof (code) === 'string') {
-	    this._code = code;
-	}
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(name) {
+    if (typeof (name) === 'string') {
+      this._name = name;
     }
-    
-    displayFullCurrency() {
-	return `${this._name} (${this._code})`;
+  }
+
+  set code(code) {
+    if (typeof (code) === 'string') {
+      this._code = code;
     }
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
