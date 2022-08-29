ff3042a1d7d4c59f532ed98c4a8af570ec58951c
AJS0191 <ashtonjamess@live.com>
2022-08-29 16:10:01 -0500

    doc

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-"""add floats with variable annotation"""
+"""Add floats with variable annotation."""
 
 
 def add(a: float, b: float) -> float:
+    """Add floats with variable annotation."""
     return a + b
