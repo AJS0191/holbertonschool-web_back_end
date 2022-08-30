ef49a60f69a0b542199a5849bfb4e76e25d4a34a
AJS0191 <ashtonjamess@live.com>
2022-08-29 19:55:54 -0500

    import from typing

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,7 +2,8 @@
 """takes list returns sum"""
 
 
-def sum_list(input_list: "list[float]") -> float:
+from typing import List
+def sum_list(input_list: List[float]) -> float:
     """takes list returns sum"""
     n = 0
     for f in input_list:
