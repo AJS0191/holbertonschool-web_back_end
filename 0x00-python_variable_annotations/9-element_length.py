1ca47d8d27f44277128d8710a3bd067c6f166fc8
AJS0191 <ashtonjamess@live.com>
2022-08-29 20:39:28 -0500

    adding annotation to given function

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""adding annotation for function"""
+
+
+from typing import Iterable, Sequence, Tuple, List
+
+
+def element_length(lst: Iterable) -> List[Tuple[Sequence, int]]:
+    """adding annotation for function"""
+    return [(i, len(i)) for i in lst]
