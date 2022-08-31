252e59c655df87e6a5dc9002e6b6d6bd5701d957
AJS0191 <ashtonjamess@live.com>
2022-08-30 22:27:41 -0500

    add an event loop to avoid no await error

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -9,8 +9,10 @@ import typing
 def measure_time(n: int, max_delay: int) -> float:
     """returns time it takes to make the random list"""
     wait_n = __import__('1-concurrent_coroutines').wait_n
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     curentT = time.perf_counter()
-    wait_n(n, max_delay)
+    result = loop.run_until_complete(wait_n(n, max_delay))
     totalTime = time.perf_counter() - curentT
     
     return totalTime / float(n)
