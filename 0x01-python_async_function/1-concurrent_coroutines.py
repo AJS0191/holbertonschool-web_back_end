8cf93a55d5d04557b17d947e47c2839813db0e75
AJS0191 <ashtonjamess@live.com>
2022-08-30 21:55:02 -0500

    typing annotation correction

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,9 +3,10 @@
 import asyncio
 import random
 import time
+import typing
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> typing.List[float]:
     """waits a random amount of time a num of times and returns list"""
     wait_random = __import__('0-basic_async_syntax').wait_random
     waitList = []
