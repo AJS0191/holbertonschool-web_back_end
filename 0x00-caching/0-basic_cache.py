0a596dbaf1a6687fae186f2b77c794f9f3415818
AJS0191 <ashtonjamess@live.com>
2022-09-07 18:50:12 -0500

    Docs

--- 0x00-caching/0-basic_cache.py
+++ 0x00-caching/0-basic_cache.py
@@ -7,13 +7,16 @@ from base_caching import BaseCaching
 class BasicCache(BaseCaching):
     """inherits for BaseCaching has no limit"""
     def __init__(self) -> None:
+        """initializes the cache"""
         super().__init__()
 
     def put(self, key, item):
+        """adds new item to cache"""
         if(key and item):
             self.cache_data[key] = item
 
     def get(self, key):
+        """returns the value of the key from cache"""
         if (key in self.cache_data):
             return self.cache_data[key]
         return None
