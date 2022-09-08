95e16ea27d87ab84e820a09f8323439983f2f5d8
AJS0191 <ashtonjamess@live.com>
2022-09-07 19:32:04 -0500

    adding first in first out cache

---
+++ 0x00-caching/1-fifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""basic_cache"""
+
+from base_caching import BaseCaching
+from collections import deque
+
+
+class FIFOCache(BaseCaching):
+    """inherits for BaseCaching has a limit"""
+    def __init__(self) -> None:
+        """initializes the cache"""
+        super().__init__()
+        self.queue = deque([])
+
+    def put(self, key, item):
+        """adds new item to cache if over max removes first in"""
+        if(key and item):
+            if(len(self.cache_data) == self.MAX_ITEMS):
+                if(key not in self.queue):
+                    rem = self.queue.popleft()
+                    del self.cache_data[rem]
+                    print(f"DISCARD: {rem}")
+            self.cache_data[key] = item
+            self.queue.append(key)
+
+    def get(self, key):
+        """returns the value of the key from cache"""
+        if (key in self.cache_data):
+            return self.cache_data[key]
+        return None
