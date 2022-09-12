2892b32784b28c4d7e99d1b4b9865fbe2cfb43e8
AJS0191 <ashtonjamess@live.com>
2022-09-12 17:00:48 -0500

    simple pagination

---
+++ 0x01-pagination/1-simple_pagination.py
@@ -0,0 +1,46 @@
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Retrieves pages and puts them into list based on size"""
+        assert type(page) == int
+        assert page > 0
+        assert type(page_size) == int
+        assert page_size > 0
+
+        dataSet = self.dataset()
+        indexRan = index_range(page, page_size)
+        showPages = []
+        size = indexRan[1]
+        start = indexRan[0]
+
+        for i in range((size * start), (size * start) + size):
+            showPages.append(dataSet[i])
+
+        return showPages
+
+
+def index_range(page: int, page_size: int):
+    """finds the index range with the page and page size"""
+    return (((page * page_size) - page_size), (page * page_size))
