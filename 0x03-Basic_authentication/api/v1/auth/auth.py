d6d089e1e6d063ee0395613ea37574cf40f7f61a
AJS0191 <ashtonjamess@live.com>
2022-09-19 20:36:36 -0500

    finish the req_auth method

--- 0x03-Basic_authentication/api/v1/auth/auth.py
+++ 0x03-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,13 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """checks if requiring auth is true"""
-        return False
+        if excluded_paths is None or path is None:
+            return True
+
+        if path in excluded_paths or path + '/' in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """sets the auth header"""
