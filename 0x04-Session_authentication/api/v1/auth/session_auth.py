719a74c68cb789a9a777cdc3b9da8797f02a1fc2
AJS0191 <ashtonjamess@live.com>
2022-09-21 17:14:24 -0500

    add getuser by session id method

--- 0x04-Session_authentication/api/v1/auth/session_auth.py
+++ 0x04-Session_authentication/api/v1/auth/session_auth.py
@@ -20,3 +20,10 @@ class SessionAuth(BasicAuth):
             sessionId = str(uuid.uuid4())
             self.user_id_by_session_id.update({sessionId: user_id})
             return sessionId
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        if not session_id or type(session_id) != str:
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
+        
