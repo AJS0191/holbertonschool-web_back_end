1c4633197de54f4a3f81cbf4db2457997fc86cdd
AJS0191 <ashtonjamess@live.com>
2022-09-21 15:42:18 -0500

    add create session

--- 0x04-Session_authentication/api/v1/auth/session_auth.py
+++ 0x04-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,19 @@
 """ Module of session_auth class
 """
 from api.v1.auth.basic_auth import BasicAuth
+import uuid
 
 
 class SessionAuth(BasicAuth):
     """class for session authorizations"""
     def __init__(self) -> None:
         super().__init__()
+        self.user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a session id for a user id"""
+        if not user_id or type(user_id) != str:
+            return None
+        else:
+            sessionId = str(uuid.uuid4())
+            self.user_id_by_session_id.update({user_id: sessionId})
