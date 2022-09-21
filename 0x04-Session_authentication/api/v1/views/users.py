7e47a2c128f5417fd5f9d3d3913dbd56bacb229a
AJS0191 <ashtonjamess@live.com>
2022-09-21 13:07:52 -0500

    ;

--- 0x04-Session_authentication/api/v1/views/users.py
+++ 0x04-Session_authentication/api/v1/views/users.py
@@ -17,7 +17,6 @@ def view_all_users() -> str:
 
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
-@app_views.route('/users/<user_id>/me', methods=['GET'], strict_slashes=False)
 def view_one_user(user_id: str = None) -> str:
     """ GET /api/v1/users/:id
     Path parameter:
