f0823b5511c81fd68975a476ca819a6473b2e660
AJS0191 <ashtonjamess@live.com>
2022-09-21 22:56:18 -0500

    add new path to ignore list

--- 0x04-Session_authentication/api/v1/app.py
+++ 0x04-Session_authentication/api/v1/app.py
@@ -32,7 +32,8 @@ def before_request():
     LIST = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
         ]
     if auth:
         if auth.require_auth(request.path, LIST):
