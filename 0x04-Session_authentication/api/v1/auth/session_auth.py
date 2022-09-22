5d98fdb618ce121c1bc227ffefb7b0cb4c705b68
AJS0191 <ashtonjamess@live.com>
2022-09-21 23:37:55 -0500

    add current user method

--- 0x04-Session_authentication/api/v1/auth/session_auth.py
+++ 0x04-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 """
 from api.v1.auth.basic_auth import BasicAuth
 import uuid
+from models.user import User
 
 
 class SessionAuth(BasicAuth):
@@ -27,3 +28,7 @@ class SessionAuth(BasicAuth):
             return None
 
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        uID = self.user_id_for_session_id(self.session_cookie(request))
+        return User.get(uID)
