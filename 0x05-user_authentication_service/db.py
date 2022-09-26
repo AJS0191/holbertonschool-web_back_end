7968a19eb539db08596a86dd9127987f2da450e0
AJS0191 <ashtonjamess@live.com>
2022-09-26 17:57:51 -0500

    add the db module

---
+++ 0x05-user_authentication_service/db.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+import uuid
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """creates and saves a user to DB"""
+        email_ = email
+        hashed_ = hashed_password
+        user = User(
+            id=str(uuid.uuid4()),
+            email=email_,
+            hashed_password=hashed_,
+            session_id=self.__session,
+            reset_token='RESET'
+        )
+        return user
