85f1b49d8b54cbbe0de3edac3caa43267510af1b
AJS0191 <ashtonjamess@live.com>
2022-09-28 22:49:53 -0500

    get user by id method

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -52,3 +52,12 @@ class Auth:
             i = _generate_uuid()
             self._db.update_user(user.id, session_id=i)
             return i
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """gets user from session_id"""
+        if not session_id:
+            return None
+        user = self._db.find_user_by(session_id=session_id)
+        if not user:
+            return None
+        return user
