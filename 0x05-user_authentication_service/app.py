e6026077284d21e325b4ed2f30d982109cf28580
AJS0191 <ashtonjamess@live.com>
2022-09-29 21:55:01 -0500

    doc

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -43,6 +43,7 @@ def login():
 
 @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
 def logout():
+    """removes session id from user and redirects"""
     session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
     if user:
