b5eb7f96893c7179d55e5e106bd0711594379715
AJS0191 <ashtonjamess@live.com>
2022-10-03 18:09:52 -0500

    using assertEQ

--- 0x00-Unittests_and_integration_tests/test_utils.py
+++ 0x00-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """the module contains unit testing"""
-from unittest import TestCase
+from unittest import TestCase, assertEq
 import parameterized
 from utils import access_nested_map
 import pytest
@@ -12,7 +12,6 @@ class TestAccessNestedMap(TestCase):
         super().__init__()
 
     @parameterized.expand
-    def test_access_nested_map(nested_map, path, expected):
+    def test_access_nested_map(self, nested_map, path, expected):
         """tests access nested map"""
-
-        assert access_nested_map(nested_map, path) == expected
+        self.assertEq(access_nested_map(nested_map, path), expected)
