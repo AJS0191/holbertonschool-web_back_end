3c40dbbc663c5f4e1af316bb14a42a5690f570a9
AJS0191 <ashtonjamess@live.com>
2022-10-03 22:19:23 -0500

    add exception test

--- 0x00-Unittests_and_integration_tests/test_utils.py
+++ 0x00-Unittests_and_integration_tests/test_utils.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """the module contains unit testing"""
-import unittest
+from unittest import TestCase
 from parameterized import parameterized
 from utils import access_nested_map
 import pytest
 
 
-class TestAccessNestedMap(unittest.TestCase):
+class TestAccessNestedMap(TestCase):
     """class that tests access nested map"""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -16,3 +16,11 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """tests access nested map"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """tests that access nested map raises keyerrors"""
+        self.assertRaises(KeyError, access_nested_map(nested_map, path))
