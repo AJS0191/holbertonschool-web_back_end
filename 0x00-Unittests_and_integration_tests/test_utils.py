b2d8f397930bb2d3b157874e8cf2ec08ce717e3a
AJS0191 <ashtonjamess@live.com>
2022-10-03 22:39:33 -0500

    give exceptions remove pytest

--- 0x00-Unittests_and_integration_tests/test_utils.py
+++ 0x00-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,6 @@
 from unittest import TestCase
 from parameterized import parameterized
 from utils import access_nested_map
-import pytest
 
 
 class TestAccessNestedMap(TestCase):
@@ -11,7 +10,7 @@ class TestAccessNestedMap(TestCase):
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         """tests access nested map"""
@@ -19,8 +18,8 @@ class TestAccessNestedMap(TestCase):
 
     @parameterized.expand([
         ({}, ("a",)),
-        ({"a": 1}, ("a", "b")),
+        ({"a": 1}, ("a", "b"))
     ])
-    def test_access_nested_map_exception(self, nested_map, path, expected):
+    def test_access_nested_map_exception(self, nested_map, path):
         """tests that access nested map raises keyerrors"""
-        self.assertRaises(KeyError, access_nested_map(nested_map, path))
+        self.assertRaises(KeyError, access_nested_map, nested_map, path)
