50b71144402276ed03042f307e790035ec9bb077
AJS0191 <ashtonjamess@live.com>
2022-10-10 12:38:43 -0500

    rename and doc

--- 0x00-redis_basic/excercise.py
+++ 0x00-redis_basic/excercise.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
+"""module containing cache class"""
 
 import uuid
 import redis
 
 
 class Cache():
+    """cache class connected to a redis server"""
     def __init__(self) -> None:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: str, bytes, int, float) -> str:
+        """stores a uuid with data in redis cache"""
         key = str(uuid.uuid3())
         self._redis.set(key, data)
