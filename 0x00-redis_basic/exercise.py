780b40eacf8f45d420dcdff89e366e6acd9af1b1
AJS0191 <ashtonjamess@live.com>
2022-10-10 19:44:41 -0500

    add get functions to cache class

--- 0x00-redis_basic/exercise.py
+++ 0x00-redis_basic/exercise.py
@@ -17,3 +17,20 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn=None) -> None:
+        """uses fn to change value of key to original datatype"""
+        val = self._redis.get(key)
+        if not val:
+            return None
+        if fn:
+            return fn(val)
+        return None
+
+    def get_str(val: bytes) -> str:
+        """changes val to str, its original data type"""
+        return val.decode('utf-8')
+
+    def get_int(val: bytes) -> int:
+        """changes val to int, its original data type"""
+        return int.from_bytes(val)
