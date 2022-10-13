a0df58b476e59420c2b0e5e732fca31231ab5b7e
AJS0191 <ashtonjamess@live.com>
2022-10-12 19:52:04 -0500

    email is unique

--- 0x01-MySQL_Advanced/0-uniq_users.sql
+++ 0x01-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Creates a table named user
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
