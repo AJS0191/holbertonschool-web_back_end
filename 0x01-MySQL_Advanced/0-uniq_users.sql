269ea119372252e09886cd2459e17deeeaa05b4c
AJS0191 <ashtonjamess@live.com>
2022-10-12 19:47:28 -0500

    is

--- 0x01-MySQL_Advanced/0-uniq_users.sql
+++ 0x01-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
---Creates a table named user
+-- Creates a table named user
 CREATE TABLE if NOT EXISTS users (
-    id int NOT NULL AUTO_INCREMENT,
-    email varchar(255) NOT NULL,
+    id int IS NOT NULL AUTO_INCREMENT,
+    email varchar(255) IS NOT NULL,
     name varchar(255)
 );
