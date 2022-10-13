fcb599fdf08ec12568e1f685f7a22d3d87110987
AJS0191 <ashtonjamess@live.com>
2022-10-12 22:45:30 -0500

    for each row

--- 0x01-MySQL_Advanced/4-store.sql
+++ 0x01-MySQL_Advanced/4-store.sql
@@ -1,3 +1,3 @@
 -- creates a trigger for items tables when a new order for an item comes in
 -- it decreases its value in the items table
-CREATE TRIGGER decrQuant AFTER INSERT ON orders UPDATE items SET items.quantity = (items.quantity - NEW.number) WHERE items.name = NEW.item_name;
+CREATE TRIGGER decrQuant AFTER INSERT ON orders FOR EACH ROW UPDATE items SET items.quantity = (items.quantity - NEW.number) WHERE items.name = NEW.item_name;
