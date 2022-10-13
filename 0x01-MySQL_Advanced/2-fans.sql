e187656af7d62fe2d609dbb8b6e8a16d2c256fe7
AJS0191 <ashtonjamess@live.com>
2022-10-12 20:34:59 -0500

    descending order

--- 0x01-MySQL_Advanced/2-fans.sql
+++ 0x01-MySQL_Advanced/2-fans.sql
@@ -1,2 +1,2 @@
 -- ranks country of origin of bands ordered fans
-SELECT origin, SUM(fans) AS nb_fans FROM metal_bands GROUP BY origin ORDER BY nb_fans;
+SELECT origin, SUM(fans) AS nb_fans FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
