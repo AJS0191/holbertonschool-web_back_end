1b9c80b6f0ededc39fb2003a071f580421e3371f
AJS0191 <ashtonjamess@live.com>
2022-10-19 23:15:20 -0500

    list

--- 0x02-NoSQL/8-all.py
+++ 0x02-NoSQL/8-all.py
@@ -11,4 +11,4 @@ db = client.my_db
 def list_all(mongo_collection):
     """lists all documents in a collection"""
     collection = db[f'{mongo_collection}']
-    return collection.find()
+    return list(collection.find())
