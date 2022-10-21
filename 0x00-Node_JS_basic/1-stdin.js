d96da8ba8397b3aef8e819ff241a7a32d2e3d3ca
AJS0191 <ashtonjamess@live.com>
2022-10-21 11:01:19 -0500

    add stdin

---
+++ 0x00-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+//takes info from user and displays
+module.exports = function nameIs(){
+  console.log("Welcome to Holberton School, what is your name?");
+  let readline = require('readline');
+  let name;
+  let rl = readline.createInterface(
+    process.stdin, process.stdout
+  );
+
+  rl.question("Welcome to Holberton School, what is your name?\n", (name) => {
+    console.log(`Your name is: ${name}`)
+  })
+
+  console.log("This important software is now closing")
+}
+nameIs();
