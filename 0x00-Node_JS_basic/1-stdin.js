58fd0bf565524aaefee75e1ab93702c56ac42c78
AJS0191 <ashtonjamess@live.com>
2022-10-21 13:47:35 -0500

    newline is back

--- 0x00-Node_JS_basic/1-stdin.js
+++ 0x00-Node_JS_basic/1-stdin.js
@@ -6,7 +6,7 @@ function nameIs() {
     process.stdin, process.stdout,
   );
 
-  rl.question('Welcome to Holberton School, what is your name?', (name) => {
+  rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
     console.log(`Your name is: ${name}`);
     console.log('This important software is now closing');
     rl.close();
