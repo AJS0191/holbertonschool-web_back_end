a2d918f9ad5a85a3c9e6d9b57df418ba21f323da
AJS0191 <ashtonjamess@live.com>
2022-10-21 13:08:20 -0500

    lint

--- 0x00-Node_JS_basic/1-stdin.js
+++ 0x00-Node_JS_basic/1-stdin.js
@@ -1,16 +1,15 @@
-//takes info from user and displays
-function nameIs(){
-  let readline = require('readline');
-  let name;
-  let rl = readline.createInterface(
+// takes info from user and displays
+function nameIs() {
+  const readline = require('readline');
+  const rl = readline.createInterface(
     process.stdin, process.stdout
   );
 
   rl.question("Welcome to Holberton School, what is your name?\n", (name) => {
-    console.log(`Your name is: ${name}`);
-    console.log("This important software is now closing");
+    console.log('Your name is: ' + name);
+    console.log('This important software is now closing');
     rl.close();
   })
   return;
-}
+};
 nameIs();
