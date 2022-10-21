f484c0b39c21316d37256dbfc01dc2aa20a6384c
AJS0191 <ashtonjamess@live.com>
2022-10-21 13:18:11 -0500

    lintr

--- 0x00-Node_JS_basic/1-stdin.js
+++ 0x00-Node_JS_basic/1-stdin.js
@@ -2,10 +2,10 @@
 function nameIs() {
   const readline = require('readline');
   const rl = readline.createInterface(
-    process.stdin, process.stdout
+    process.stdin, process.stdout,
   );
 
-  rl.question("Welcome to Holberton School, what is your name?\n", (name) => {
+  rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
     console.log('Your name is: ' + name);
     console.log('This important software is now closing');
     rl.close();
