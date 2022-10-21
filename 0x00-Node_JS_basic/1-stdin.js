83e4ade26aed861da11189fa098834efe71029ec
AJS0191 <ashtonjamess@live.com>
2022-10-21 13:35:44 -0500

    no newline

--- 0x00-Node_JS_basic/1-stdin.js
+++ 0x00-Node_JS_basic/1-stdin.js
@@ -8,7 +8,7 @@ function nameIs() {
 
   console.log(process.argv);
 
-  rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  rl.question('Welcome to Holberton School, what is your name?', (name) => {
     console.log(`Your name is: ${name}`);
     console.log('This important software is now closing');
     rl.close();
