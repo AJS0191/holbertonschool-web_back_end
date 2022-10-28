dae3ac17a939c34d98b7f8828f3df959fadebba1
AJS0191 <ashtonjamess@live.com>
2022-10-28 13:51:46 -0500

    payment plan part 5

---
+++ 0x01-unittests_in_js/5-payment.test.js
@@ -0,0 +1,19 @@
+
+const { expect } = require("chai");
+const sinon = require('sinon')
+const Utils = require("./utils");
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe("compare two add functions", function() {
+  it("checks equality", function() {
+    const stubbedFunc = sinon.spy(Utils, 'calculateNumber').returns(10);
+    let spy = sinon.spy(sendPaymentRequestToApi)
+    beforeEach( () => {
+      sendPaymentRequestToApi(100, 20);
+    })
+    afterEach( () => {
+      spy.restore();
+    })
+    expect(sendPaymentRequestToApi.calledWith(100, 20)).to.be.true;
+    expect(sendPaymentRequestToApi.calledWith(10, 2)).to.equal(20);
+  })});
