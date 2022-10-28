880a6a8b9b43212cbf0f2a61b821d98d29a3fbe3
AJS0191 <ashtonjamess@live.com>
2022-10-28 09:44:01 -0500

    fix tests to new prototype

--- 0x01-unittests_in_js/1-calcul.test.js
+++ 0x01-unittests_in_js/1-calcul.test.js
@@ -3,58 +3,58 @@ const calculateNumber = require("./1-calcul")
 
 describe("simple add rounded test", function() {
   it("checks equality", function() {
-    assert.equal(calculateNumber(1,2, 'SUM'), 3)
-    assert.equal(calculateNumber(1,2.5, 'SUM'), 4)
-    assert.equal(calculateNumber(1,2.2, 'SUM'), 3)
-    assert.equal(calculateNumber(1,0.2, 'SUM'), 1)
-    assert.equal(calculateNumber(1,0.5, 'SUM'), 2)
+    assert.equal(calculateNumber('SUM',1,2), 3)
+    assert.equal(calculateNumber('SUM',1,2.5), 4)
+    assert.equal(calculateNumber('SUM',1,2.2), 3)
+    assert.equal(calculateNumber('SUM',1,0.2), 1)
+    assert.equal(calculateNumber('SUM',1,0.5), 2)
   })
 
   it("checks 0s", function() {
-    assert.equal(calculateNumber(0,0.2, 'SUM'), 0)
-    assert.equal(calculateNumber(0,0.5, 'SUM'), 1)
+    assert.equal(calculateNumber('SUM',0,0.2), 0)
+    assert.equal(calculateNumber('SUM',0,0.5), 1)
   })
 
   it("checks negs", function() {
-    assert.equal(calculateNumber(1,-0.2, 'SUM'), 1)
-    assert.equal(calculateNumber(1,-0.5, 'SUM'), 0)
+    assert.equal(calculateNumber('SUM',1,-0.2), 1)
+    assert.equal(calculateNumber('SUM',1,-0.5), 0)
   })
 })
 describe("simple 'SUBTRACT' rounded test", function() {
   it("checks equality", function() {
-    assert.equal(calculateNumber(1,2, 'SUBTRACT'), 1)
-    assert.equal(calculateNumber(1,2.5, 'SUBTRACT'), 2)
-    assert.equal(calculateNumber(1,2.2, 'SUBTRACT'), 1)
-    assert.equal(calculateNumber(1,0.2, 'SUBTRACT'), -1)
-    assert.equal(calculateNumber(1,0.5, 'SUBTRACT'), 0)
+    assert.equal(calculateNumber('SUBTRACT',1,2), 1)
+    assert.equal(calculateNumber('SUBTRACT',1,2.5), 2)
+    assert.equal(calculateNumber('SUBTRACT',1,2.2), 1)
+    assert.equal(calculateNumber('SUBTRACT',1,0.2), -1)
+    assert.equal(calculateNumber('SUBTRACT',1,0.5), 0)
   })
 
   it("checks 0s", function() {
-    assert.equal(calculateNumber(0,0.2, 'SUBTRACT'), 0)
-    assert.equal(calculateNumber(0,0.5, 'SUBTRACT'), 1)
+    assert.equal(calculateNumber('SUBTRACT',0,0.2), 0)
+    assert.equal(calculateNumber('SUBTRACT',0,0.5), 1)
   })
 
   it("checks negs", function() {
-    assert.equal(calculateNumber(1,-0.2, 'SUBTRACT'), -1)
-    assert.equal(calculateNumber(1,-0.5, 'SUBTRACT'), -2)
+    assert.equal(calculateNumber('SUBTRACT',1,-0.2), -1)
+    assert.equal(calculateNumber('SUBTRACT',1,-0.5), -2)
   })
 })
 describe("simple 'DIVIDE' rounded test", function() {
   it("checks equality", function() {
-    assert.equal(calculateNumber(1,2, 'DIVIDE'), 0.5)
-    assert.equal(calculateNumber(1,2.5, 'DIVIDE'), 0.33)
-    assert.equal(calculateNumber(1,2.2, 'DIVIDE'), 0.5)
-    assert.equal(calculateNumber(1,0.2, 'DIVIDE'), 'Error')
-    assert.equal(calculateNumber(1,0.5, 'DIVIDE'), 1)
+    assert.equal(calculateNumber('DIVIDE',1,2), 0.5)
+    assert.equal(calculateNumber('DIVIDE',1,2.5), 0.33)
+    assert.equal(calculateNumber('DIVIDE',1,2.2), 0.5)
+    assert.equal(calculateNumber('DIVIDE',1,0.2), 'Error')
+    assert.equal(calculateNumber('DIVIDE',1,0.5), 1)
   })
 
   it("checks 0s", function() {
-    assert.equal(calculateNumber(0,0.2, 'DIVIDE'), 'Error')
-    assert.equal(calculateNumber(0,0.5, 'DIVIDE'), 0)
+    assert.equal(calculateNumber('DIVIDE',0,0.2), 'Error')
+    assert.equal(calculateNumber('DIVIDE',0,0.5), 0)
   })
 
   it("checks negs", function() {
-    assert.equal(calculateNumber(1,-0.2, 'DIVIDE'), 'Error')
-    assert.equal(calculateNumber(1,-0.5, 'DIVIDE'), -1)
+    assert.equal(calculateNumber('DIVIDE',1,-0.2), 'Error')
+    assert.equal(calculateNumber('DIVIDE',1,-0.5), -1)
   })
 })
