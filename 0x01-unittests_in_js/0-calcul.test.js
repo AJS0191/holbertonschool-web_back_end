08b0eeb6792e60a81012a5ca6be685c18fc22ada
AJS0191 <ashtonjamess@live.com>
2022-10-28 09:17:22 -0500

    new test

--- 0x01-unittests_in_js/0-calcul.test.js
+++ 0x01-unittests_in_js/0-calcul.test.js
@@ -8,6 +8,8 @@ describe("simple add rounded test", function() {
     assert.equal(calculateNumber(1,2.2), 3)
     assert.equal(calculateNumber(1,0.2), 1)
     assert.equal(calculateNumber(1,0.5), 2)
+    assert.equal(calculateNumber(0.2,1), 1)
+    assert.equal(calculateNumber(0.5,1), 2)
   })
 
   it("checks 0s", function() {
