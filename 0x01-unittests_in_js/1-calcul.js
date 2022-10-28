fc8247c3ad4ba676bda6f0ddd59173390710d1ff
AJS0191 <ashtonjamess@live.com>
2022-10-28 09:27:57 -0500

    case switch

--- 0x01-unittests_in_js/1-calcul.js
+++ 0x01-unittests_in_js/1-calcul.js
@@ -1,18 +1,16 @@
 function calculateNumber(a, b, type) {
-  if (type == 'SUM') {
-    return Math.round(a) + Math.round(b)
-  }
-  if (type == 'SUBTRACT') {
+  switch (type){
+    case 'SUM':  
+      return Math.round(a) + Math.round(b)
+    case 'SUBTRACT':
     return Math.round(b) - Math.round(a)
-  }
-  if (type == 'DIVIDE') {
-    if (Math.round(b) == 0) {
-      return 'Error'
-    } else {
-      c = Math.round(a) / Math.round(b)
-      return Math.round(c)
+  
+    case 'DIVIDE':
+      if (Math.round(b) == 0) {
+        return 'Error'
+      } else {
+        c = Math.round(a) / Math.round(b)
+        return Math.round(c)
     }
-  }
-}
-
-module.exports = calculateNumber
+  }}
+    module.exports = calculateNumber
