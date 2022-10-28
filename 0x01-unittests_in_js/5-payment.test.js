da885748813b446495b1c2a6842ea3d2b0e37208
AJS0191 <ashtonjamess@live.com>
2022-10-28 14:00:43 -0500

    lol

--- 0x01-unittests_in_js/5-payment.test.js
+++ 0x01-unittests_in_js/5-payment.test.js
@@ -11,7 +11,7 @@ describe("compare two add functions", function() {
       sendPaymentRequestToApi(100, 20);
     })
     afterEach( () => {
-      pass
+      spy = spy;
     })
     expect(sendPaymentRequestToApi(100, 20)).to.equal(120);
     expect(sendPaymentRequestToApi(10, 2)).to.equal(12);
