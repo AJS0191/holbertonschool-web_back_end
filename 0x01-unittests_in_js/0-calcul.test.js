c5dacca3b89469ca64e8f52b7049c0e0e9f81da6
AJS0191 <ashtonjamess@live.com>
2022-10-27 21:36:05 -0500

    add tests

--- 0x01-unittests_in_js/0-calcul.test.js
+++ 0x01-unittests_in_js/0-calcul.test.js
@@ -4,5 +4,9 @@ const calculateNumber = require("./0-calcul")
 describe("simple add rounded test", function() {
   it("checks equality", function() {
     assert.equal(calculateNumber(1,2), 3)
+    assert.equal(calculateNumber(1,2.5), 4)
+    assert.equal(calculateNumber(1,2.2), 3)
+    assert.equal(calculateNumber(1,0.2), 1)
+    assert.equal(calculateNumber(1,0.5), 2)
   })
 })
