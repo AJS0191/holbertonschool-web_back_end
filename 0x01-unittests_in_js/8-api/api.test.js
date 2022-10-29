c4fb27933f160b98998e74650f8459a3da347203
AJS0191 <ashtonjamess@live.com>
2022-10-28 21:05:28 -0500

    api tests

---
+++ 0x01-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,13 @@
+request = require('request')
+const chai = require('chai')
+const expect = chai.expect
+
+describe('api tests', function() {
+  it('testing get method'), () => new Promise((done) => {
+    request('http://localhost:7865/'), (error, response, body) => {
+      expect(response.statusCode).to.equal(200)
+      expect(response.request.method).to.equal('GET')
+      done();
+    }
+  })
+  });
