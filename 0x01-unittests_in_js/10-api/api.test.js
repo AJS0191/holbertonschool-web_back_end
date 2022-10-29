01c003584e2a160506db1d171dd90f206df26285
AJS0191 <ashtonjamess@live.com>
2022-10-28 23:42:58 -0500

    t

--- 0x01-unittests_in_js/10-api/api.test.js
+++ 0x01-unittests_in_js/10-api/api.test.js
@@ -31,7 +31,7 @@ describe('api tests', function() {
     request('http://localhost:7865/available_payments', (error, response, body) => {
       expect(response.statusCode).to.equal(200)
       expect(response.request.method).to.equal('GET')
-      expect(body).to.equal({ payment_methods: { …(2) } })
+      expect(body).to.equal('{ payment_methods: { …(2) } }')
       done();
     })
   }))
