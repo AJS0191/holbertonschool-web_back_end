8615787fcc9a78c4e5006d538f693ca1d1153980
AJS0191 <ashtonjamess@live.com>
2022-11-04 13:58:34 -0500

    give it a shot

---
+++ 0x02-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,19 @@
+import { createClient } from 'redis';
+import print from 'redis'
+
+const client = createClient();
+
+client.on('error', (err) => { console.log("Redis client not connected to server: " + err)});
+client.on('connect', () => { console.log("Redis client connected to the server")});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, print)
+};
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName)
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFransisco', '100');
+displaySchoolValue('HolbertonSanFransisco');
