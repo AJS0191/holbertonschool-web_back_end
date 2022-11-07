63bdcc3c0ff5a6401edbb0e311c5053642c661f7
AJS0191 <ashtonjamess@live.com>
2022-11-07 14:47:50 -0600

    log the correct message

--- 0x02-queuing_system_in_js/5-subscriber.js
+++ 0x02-queuing_system_in_js/5-subscriber.js
@@ -5,11 +5,11 @@ const client = createClient();
 
 client.on('error', (err) => { console.log("Redis client not connected to server: " + err)});
 client.on('connect', () => { console.log("Redis client connected to the server")});
-client.on('message', (msg) => {
+client.on('message', (chan, msg) => {
   if (msg == 'KILL_SERVER') {
     client.quit();
   }
   console.log(msg);
 })
 
-client.subscribe('holberton school channel')
+client.subscribe('holberton school channel') 
