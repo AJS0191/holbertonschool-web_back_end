c178b2027ae6801530edddd113785c825f6c0498
AJS0191 <ashtonjamess@live.com>
2022-11-07 14:03:27 -0600

    subscriber script

---
+++ 0x02-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,15 @@
+import { createClient } from 'redis';
+import { print } from 'redis'
+
+const client = createClient();
+
+client.on('error', (err) => { console.log("Redis client not connected to server: " + err)});
+client.on('connect', () => { console.log("Redis client connected to the server")});
+client.on('message', (msg) => {
+  if (msg == 'KILL_SERVER') {
+    client.quit();
+  }
+  console.log(msg);
+})
+
+client.subscribe('holberton school channel')
