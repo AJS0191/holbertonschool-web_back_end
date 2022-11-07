51721eb198a8320110ea4eda42ee6c842a96c868
AJS0191 <ashtonjamess@live.com>
2022-11-07 12:03:30 -0600

    doe each hashset seperately

--- 0x02-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x02-queuing_system_in_js/4-redis_advanced_op.js
@@ -6,14 +6,12 @@ const client = createClient();
 client.on('error', (err) => { console.log("Redis client not connected to server: " + err)});
 client.on('connect', () => { console.log("Redis client connected to the server")});
 
-const schoolList = {
-  Portland: '50',
-  Seattle: '80',
-  'New York': '20',
-  Bogota: '20',
-  Cali: '40',
-  Paris: '2'
-};
 
-client.hset('HolbertonSchools', schoolList, print);
+client.hset('HolbertonSchools', 'Portland', '50', print);
+client.hset('HolbertonSchools', 'Seattle', '80', print);
+client.hset('HolbertonSchools', 'New York', '20', print);
+client.hset('HolbertonSchools', 'Bogota', '20', print);
+client.hset('HolbertonSchools', 'Cali', '40', print);
+client.hset('HolbertonSchools', 'Paris', '2', print);
+
 client.hgetall('HolbertonSchools');
