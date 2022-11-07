41c0f5fab427848528fe2e87e24943d83e955ed1
AJS0191 <ashtonjamess@live.com>
2022-11-07 14:33:33 -0600

    adding publisher client script

---
+++ 0x02-queuing_system_in_js/5-publisher.js
@@ -0,0 +1,25 @@
+import { setMaxIdleHTTPParsers } from 'http';
+import { createClient } from 'redis';
+import { print } from 'redis'
+
+const client = createClient();
+
+client.on('error', (err) => { console.log("Redis client not connected to server: " + err)});
+client.on('connect', () => { console.log("Redis client connected to the server")});
+
+async function publishMessage(message, time) {
+  await sleep(time);
+  console.log(`About to send ${message}`);
+  client.publish('holberton school channel', message);
+};
+
+function sleep(tm) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, tm)
+  })
+};
+
+publishMessage("Holberton Student #1 starts course", 100);
+publishMessage("Holberton Student #2 starts course", 200);
+publishMessage("KILL_SERVER", 300);
+publishMessage("Holberton Student #3 starts course", 400);
