784e73399efbeb77fea5986ed43d2cd6cdf4019f
AJS0191 <ashtonjamess@live.com>
2022-11-07 20:12:37 -0600

    var name change

--- 0x02-queuing_system_in_js/6-job_creator.js
+++ 0x02-queuing_system_in_js/6-job_creator.js
@@ -1,19 +1,19 @@
 var kue = require('kue');
 var queue = kue.createQueue()
 
-jobData = {
+var jobData = {
   phoneNumber: 'string',
   message: 'string',
 }
 
-push_notification_code = queue.create(jobData).save( function(err){
+var push_notification_code = queue.create(jobData).save( function(err){
   console.log(`Notification job created: ${push_notification_code.id}`)
 })
 
-job.on('complete', function(result){
+push_notification_code.on('complete', function(result){
   console.log('Notification job completed');
 })
 
-job.on('failed', function(err){
+push_notification_code.on('failed', function(err){
   console.log('Notification job failed')
 })
