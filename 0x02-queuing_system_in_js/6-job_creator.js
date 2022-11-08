baa23dc68a1e114b0c990484153dc4933475dbcd
AJS0191 <ashtonjamess@live.com>
2022-11-07 20:06:08 -0600

    kue job creator

---
+++ 0x02-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,19 @@
+var kue = require('kue');
+queue = kue.createQueue()
+
+jobData = {
+  phoneNumber: string,
+  message: string,
+}
+
+push_notification_code = queue.create(jobData).save( function(err){
+  console.log(`Notification job created: ${push_notification_code.id}`)
+})
+
+job.on('complete', function(result){
+  console.log('Notification job completed');
+})
+
+job.on('failed', function(err){
+  console.log('Notification job failed')
+})
