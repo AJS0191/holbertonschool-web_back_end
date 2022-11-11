012a675b8ec943a5805d3012c0d8b588c4e9a09a
AJS0191 <ashtonjamess@live.com>
2022-11-11 14:30:58 -0600

    split job data

--- 0x02-queuing_system_in_js/6-job_processor.js
+++ 0x02-queuing_system_in_js/6-job_processor.js
@@ -3,12 +3,10 @@ var queue = kue.createQueue()
 
 function sendNotification(phoneNumber, message) {
     console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
-    done();
 }
 
 queue.process('push_notification_code', (job, done) => {
-    console.log(job);
-    console.log(job.data);
-    console.log(job.data.to)
-    sendNotification(job.data.to, done);
+    const { phoneNumber, message } = job.data
+    sendNotification(phoneNumber, message);
+    done();
 })
