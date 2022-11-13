b5d6c37eb248818bada52e4c2478211d2546ca09
AJS0191 <ashtonjamess@live.com>
2022-11-13 10:34:19 -0600

    fix if statement for blacklist

--- 0x02-queuing_system_in_js/7-job_processor.js
+++ 0x02-queuing_system_in_js/7-job_processor.js
@@ -5,7 +5,7 @@ const blacklisted = ['4153518780', '4153518781']
 
 function sendNotification(phoneNumber, message, job, done) {
     job.progress(0, 100);
-    if (phoneNumber in blacklisted){
+    if (blacklisted.includes(phoneNumber)){
         throw new Error(`Phone number ${phoneNumber} is blacklisted`);
     }
     job.progress(50, 100);
