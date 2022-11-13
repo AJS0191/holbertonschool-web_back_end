d3259cc6932ae3a0aef35d3160748aec9bd532e7
AJS0191 <ashtonjamess@live.com>
2022-11-13 17:40:40 -0600

    return dont throw!

--- 0x02-queuing_system_in_js/7-job_processor.js
+++ 0x02-queuing_system_in_js/7-job_processor.js
@@ -6,7 +6,7 @@ const blacklisted = ['4153518780', '4153518781']
 function sendNotification(phoneNumber, message, job, done) {
     job.progress(0, 100);
     if (blacklisted.includes(phoneNumber)){
-        throw new Error(`Phone number ${phoneNumber} is blacklisted`);
+        return new Error(`Phone number ${phoneNumber} is blacklisted`);
     }
     job.progress(50, 100);
     console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
