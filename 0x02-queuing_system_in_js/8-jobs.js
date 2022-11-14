d838b90444b9ea98a0e017a6a021395c8fe9456a
AJS0191 <ashtonjamess@live.com>
2022-11-13 19:33:02 -0600

    add 8-jobs and its test

---
+++ 0x02-queuing_system_in_js/8-jobs.js
@@ -0,0 +1,19 @@
+
+function createPushNotificationsJobs(jobs, queue){    
+  if (typeof(jobs) != Array){return new Error('Jobs is not an array')};
+  jobs.forEach(job => {
+      var push_notification_code3 = queue.create('push_notification_code3', {
+        phoneNumber: job.phoneNumber,
+        message: job.message,
+      }).save( function(err){
+        console.log(`Notification job created: ${push_notification_code3.id}`)
+        }).on('complete', function(result){
+          console.log(`Notification job ${push_notification_code3.id} completed`);
+        }).on('failed', function(err){
+          console.log(`Notification job ${push_notification_code3.id} failed: ${err}`)
+        }).on('progress', function(progress, data){
+          console.log(`Notification job ${push_notification_code3.id} ${progress}% complete`)
+        })
+  });
+}
+module.exports = createPushNotificationsJobs
