cc5515488b5efd61655e8ce5a5cdca5170699473
AJS0191 <ashtonjamess@live.com>
2022-11-13 19:53:10 -0600

    comment test

--- 0x02-queuing_system_in_js/8-job.js
+++ 0x02-queuing_system_in_js/8-job.js
@@ -1,7 +1,7 @@
 
 function createPushNotificationsJobs(jobs, queue){ 
   console.log('hit function')   
-  if (typeof(jobs) != Array){return new Error('Jobs is not an array')};
+  //if (typeof(jobs) != Array){return new Error('Jobs is not an array')};
   jobs.forEach(job => {
       var push_notification_code3 = queue.create('push_notification_code3', {
         phoneNumber: job.phoneNumber,
