09d1769f01833544551d2f5ad43cf1971e64b7a4
AJS0191 <ashtonjamess@live.com>
2022-11-29 11:01:47 -0600

    test get_json

--- 0x00-Unittests_and_integration_tests/test_utils.py
+++ 0x00-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,9 @@
 """the module contains unit testing"""
 from unittest import TestCase
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import patch
+import utils
 
 
 class TestAccessNestedMap(TestCase):
@@ -23,3 +25,16 @@ class TestAccessNestedMap(TestCase):
     def test_access_nested_map_exception(self, nested_map, path):
         """tests that access nested map raises keyerrors"""
         self.assertRaises(KeyError, access_nested_map, nested_map, path)
+
+    class TestGetJson(TestCase):
+        """testing with mock http calls"""
+        @parameterized.expand([
+            "http://example.com", {"payload": True},
+            "http://holberton.io", {"payload": False}
+        ])
+        def test_get_json(self, tUrl, tPayload):
+            """testing that get_json gets the correct payload"""
+            with patch.object(utils.requests, 'get',
+                              return_value=tPayload) as mGet:
+                self.assertEqual(get_json(tUrl), tPayload)
+                mGet.assert_called_once_with(tUrl)
